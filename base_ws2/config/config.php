8f18587a95ae52f595be5d3d1dea905717c60629
loan2806 <trantkloan2006@gmail.com>
2026-03-27 10:38:45 +0700

    add fruit_shop.sql

--- base_ws2/config/config.php
+++ base_ws2/config/config.php
@@ -5,14 +5,14 @@ if (!defined('BASE_PATH')) {
     define('BASE_PATH', dirname(__DIR__)); // Đường dẫn tuyệt đối tới thư mục gốc của dự án
 }
 if (!defined('BASE_URL')) {
-    define('BASE_URL', '/website_quan_ly_tour/'); // URL cơ bản của dự án(Lưu ý cấp độ trong htdocs hoặc www)
+    define('BASE_URL', '/base_ws2/base_ws2/'); // URL cơ bản của dự án(Lưu ý cấp độ trong htdocs hoặc www)
 }
 
 // Cấu hình cơ bản cho kết nối CSDL
 return [
     'db' => [
         'host' => 'localhost', // Host cơ sở dữ liệu
-        'name' => 'website_ql_tour', // Tên cơ sở dữ liệu
+        'name' => 'fuit_shop_agile', // Tên cơ sở dữ liệu
         'user' => 'root', // Tên người dùng
         'pass' => '', // Mật khẩu
         'charset' => 'utf8mb4', // Mã hóa
